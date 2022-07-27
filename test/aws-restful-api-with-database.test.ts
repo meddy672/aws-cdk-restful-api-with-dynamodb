e31bbb9f5f0f9a085cd6a721c3936b688037bae3
Matthew Eddy <meddy672@gmail.com>
2022-07-27 07:39:33 -0400

    Initial commit

---
+++ test/aws-restful-api-with-database.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsRestfulApiWithDatabase from '../lib/aws-restful-api-with-database-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-restful-api-with-database-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsRestfulApiWithDatabase.AwsRestfulApiWithDatabaseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
